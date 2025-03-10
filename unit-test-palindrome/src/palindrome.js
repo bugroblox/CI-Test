c7bf690497efb810ee8a7a677f0d901381e90d85
Ryan <bowryf1026@gmail.com>
2025-03-10 11:54:50 -0700

    palindrome 2

--- unit-test-palindrome/src/palindrome.js
+++ unit-test-palindrome/src/palindrome.js
@@ -1,5 +1,5 @@
 function isPalindrome(str) {
-    const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, "");
     return cleaned === cleaned.split('').reverse().join('');
 }
 
